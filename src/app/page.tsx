89ce84f5117c61fff19d8688383dcfcb3ea91232
razod <masonboulier@gmail.com>
2025-09-04 12:49:05 -0500

    create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Landing from "@/components/Landing";
 import Messages from "@/components/Messages";
 import Image from "next/image";
 
+const landing = true;
+
 export default function Home() {
-  return (
-    <div className="backdrop flex items-center justify-center h-screen overflow-hidden">
-      <div className="bg h-[100vh] w-[450px] flex flex-col items-center justify-center overflow-hidden">
-        <div className="h-full w-full overflow-y-auto flex flex-col items-center">
-          <Hero />
-          <Messages />
-          <Footer />
+  if (landing) {
+    return (
+      <>
+        <Landing />
+      </>
+    )
+  } else {
+    return (
+      <div className="backdrop flex items-center justify-center h-screen overflow-hidden">
+        <div className="bg h-[100vh] w-[450px] flex flex-col items-center justify-center overflow-hidden">
+          <div className="h-full w-full overflow-y-auto flex flex-col items-center">
+            <Hero />
+            <Messages />
+            <Footer />
+          </div>
         </div>
-      </div>
-    </div >
-  );
+      </div >
+    );
+  }
 }
