859a7efc1e9ac0d1afc654752dfc7924a4145b82
razod <masonboulier@gmail.com>
2025-09-08 20:23:04 -0500

    initialize supabase

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,23 +7,23 @@ import Image from "next/image";
 const landing = false;
 
 export default function Home() {
-  if (landing) {
-    return (
-      <>
-        <Landing />
-      </>
-    )
-  } else {
-    return (
-      <div className="backdrop flex items-center justify-center h-screen overflow-hidden">
-        <div className="bg h-[100vh] w-[450px] flex flex-col items-center justify-center overflow-hidden">
-          <div className="h-full w-full overflow-y-auto flex flex-col items-center">
-            <Hero />
-            <Messages />
-            <Footer />
-          </div>
-        </div>
-      </div >
-    );
-  }
+    if (landing) {
+        return (
+            <>
+                <Landing />
+            </>
+        )
+    } else {
+        return (
+            <div className="backdrop flex items-center justify-center h-screen overflow-hidden">
+                <div className="bg h-[100vh] w-[450px] flex flex-col items-center justify-center overflow-hidden">
+                    <div className="h-full w-full overflow-y-auto flex flex-col items-center">
+                        <Hero />
+                        <Messages />
+                        <Footer />
+                    </div>
+                </div>
+            </div >
+        );
+    }
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="backdrop flex items-center justify-center h-screen overflow-hidden">
+            <div className="bg h-[100vh] w-[450px] flex flex-col items-center justify-center overflow-hidden">
+                <div className='w-6/7 bg-white mt-5 border-1 border-black rounded-xl text-center ds'>
+                    <div>
+                        <h1 className='pt-5 text-black font-bold text-xl'>Error 722 (404)</h1>
+                        <p className='pt-2 pb-5 text-gray-500 text-sm w-5/6 mx-auto'> This page could not be found! <br /> 🐰💙🩷💛💚💜</p>
+                        <a href="/" className='msg-bg py-3 px-4 text-black border-1 border-black font-semibold rounded-4xl ds cursor-pointer no-underline'>
+                            Home
+                        </a>
+                        <p className='pt-5 pb-5 text-gray-500 text-xs w-5/6 mx-auto'>Made with ❤️ by <a href="https://x.com/sun4nce">@sun4nce</a></p>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default NotFound;
