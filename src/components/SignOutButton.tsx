1a7303671c6a3171f89cabc01ee0c7656397ce1a
razod <masonboulier@gmail.com>
2025-09-18 10:16:39 -0500

    fix sign out errors

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,11 +1,13 @@
+'use client'
 import React from 'react'
-import { createClient } from '@/lib/supabase/server'
+import { createClient } from '@/lib/supabase/client'
 
 
-const SignOutButton = async () => {
-    const supabase = await createClient()
+const SignOutButton = () => {
+    const supabase = createClient()
     const handleSignOut = async () => {
         await supabase.auth.signOut()
+        window.location.reload()
     }
     return (
         <button onClick={handleSignOut} className='msg-bg py-2 px-3 text-black border-1 border-black text-sm font-semibold rounded-xl ds cursor-pointer no-underline'>
