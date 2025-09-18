15beb5c969348752677d350ebc077ea802af2f8d
razod <masonboulier@gmail.com>
2025-09-18 09:54:35 -0500

    Fix wording bases on # likes

--- src/components/LikeControl.tsx
+++ src/components/LikeControl.tsx
@@ -37,7 +37,7 @@ const LikeControl: React.FC<Props> = ({ messageId, initialLikes, initiallyLiked 
             <button onClick={toggleLike} disabled={isPending} className={'text-sm rounded-xl px-1 py-1 text-black ' + (liked ? 'bg-red-200' : 'bg-white')} aria-pressed={liked}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-thumbs-up-icon lucide-thumbs-up"><path d="M7 10v12" /><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z" /></svg>
             </button>
-            <p className='text-black text-xs'>{likes} likes</p>
+            <p className='text-black text-xs'>{likes} {(likes != 1 ? 'likes' : 'like')}</p>
         </div>
     )
 }
