bc3e94cc980bbbb99f6ad1fbe5f693019897dd1e
razod <masonboulier@gmail.com>
2025-09-18 18:25:57 -0500

    Change like icon to a heart

--- src/components/Messages/LikeControl.tsx
+++ src/components/Messages/LikeControl.tsx
@@ -34,8 +34,10 @@ const LikeControl: React.FC<Props> = ({ messageId, initialLikes, initiallyLiked 
 
     return (
         <div className='pt-2 pb-3 w-11/12 mx-auto flex items-center gap-2'>
-            <button onClick={toggleLike} disabled={isPending} className={'text-sm rounded-xl px-1 py-1 text-black ' + (liked ? 'bg-red-200' : 'bg-white')} aria-pressed={liked}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-thumbs-up-icon lucide-thumbs-up"><path d="M7 10v12" /><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z" /></svg>
+            <button onClick={toggleLike} disabled={isPending} className={'text-sm py-1 px-1 rounded-xl text-black bg-white '} aria-pressed={liked}>
+                <svg width="16" height="16" viewBox="0 0 24 24" fill={liked ? "#fc8181" : "none"} stroke={liked ? "black" : "currentColor"} strokeWidth={liked ? "1" : "1.5"} strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+                </svg>
             </button>
             <p className='text-black text-xs'>{likes} {(likes != 1 ? 'likes' : 'like')}</p>
         </div>
